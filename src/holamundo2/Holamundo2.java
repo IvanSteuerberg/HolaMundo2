c2919da1431088033ce6977534bd11ee6676d99b
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2019-10-11 13:32:36 +0200

    Primer commit de hola mundo

---
+++ src/holamundo2/Holamundo2.java
@@ -0,0 +1,12 @@
+
+package holamundo2;
+//@author irodriguezsteuerberg
+ 
+public class Holamundo2 {
+
+    public static void main(String[] args) {
+    System.out.println("Hola mundo");
+            
+    }
+    
+}
