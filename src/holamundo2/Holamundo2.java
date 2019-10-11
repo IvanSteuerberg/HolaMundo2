bf1dca099a54083616a1bc0002bd7320321638a5
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2019-10-11 13:34:53 +0200

    Segundo commit del programa hola mundo, ahora muestra Hola mundo 2

--- src/holamundo2/Holamundo2.java
+++ src/holamundo2/Holamundo2.java
@@ -5,7 +5,7 @@ package holamundo2;
 public class Holamundo2 {
 
     public static void main(String[] args) {
-    System.out.println("Hola mundo");
+    System.out.println("Hola mundo 2");
             
     }
     
